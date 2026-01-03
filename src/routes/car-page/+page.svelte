a3e5249361168bcaf2184eb52f4f401b46c5a25a
garysteele777 <155407816+garysteele777@users.noreply.github.com>
2026-01-02 19:24:16 -0600

    commit before copying car-mode page from user-frontend

--- src/routes/car-page/+page.svelte
+++ src/routes/car-page/+page.svelte
@@ -1,54 +1,66 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
+    import {onMount, onDestroy} from 'svelte';
 
-	import CarModeHeader from '$lib/components/car/CarModeHeader.svelte';
+    import CarModeHeader from '$lib/components/car/CarModeHeader.svelte';
 
-	import {
-		currentSelection,
-		status
-	} from '$lib/carmode/CarMode.store';
+    import {
+        currentSelection,
+        status
+    } from '$lib/carmode/CarMode.store';
 
-	import { buildSelectionFromUrl } from '$lib/carmode/CarMode.url';
-	import { loadForSelection } from '$lib/carmode/CarMode.loader';
-	import { stopPlaybackPolling } from '$lib/carmode/CarMode.poller';
-	import { clearAllPlayback } from '$lib/carmode/CarMode.player';
+    import {buildSelectionFromUrl} from '$lib/carmode/CarMode.url';
+    import {loadForSelection} from '$lib/carmode/CarMode.loader';
+    import {stopPlaybackPolling} from '$lib/carmode/CarMode.poller';
+    import {clearAllPlayback} from '$lib/carmode/CarMode.player';
 
-	onMount(async () => {
-		console.log('🚗 CarMode bootstrap START');
+    import CarModeTrackMeta from '$lib/components/car/CarModeTrackMeta.svelte';
+    import {currentTrack} from '$lib/carmode/CarMode.store';
 
-		const url = new URL(window.location.href);
-		const sel = buildSelectionFromUrl(url);
 
-		currentSelection.set(sel);
-		await loadForSelection(sel);
+    onMount(async () => {
+        console.log('🚗 CarMode bootstrap START');
 
-		console.log('🚗 CarMode bootstrap DONE');
-	});
+        const url = new URL(window.location.href);
+        const sel = buildSelectionFromUrl(url);
 
-	onDestroy(() => {
-		stopPlaybackPolling();
-		void clearAllPlayback();
-	});
+        currentSelection.set(sel);
+        await loadForSelection(sel);
+
+        console.log('🚗 CarMode bootstrap DONE');
+    });
+
+    onDestroy(() => {
+        stopPlaybackPolling();
+        void clearAllPlayback();
+    });
 </script>
 
 {#if $currentSelection}
-	<CarModeHeader
-		decade={$currentSelection.context?.decade}
-		genre={$currentSelection.context?.genre}
-		collection={$currentSelection.context?.collection_slug}
-		mode={$currentSelection.mode}
-		language={$currentSelection.language}
-		voices={$currentSelection.voices}
-		startRank={$currentSelection.startRank}
-		endRank={$currentSelection.endRank}
-		playbackOrder={$currentSelection.playbackOrder}
-		voicePlayMode={$currentSelection.voicePlayMode}
-		pauseMode={$currentSelection.pauseMode}
-		categoryMode="single"
-	/>
+    <CarModeHeader
+            decade={$currentSelection.context?.decade}
+            genre={$currentSelection.context?.genre}
+            collection={$currentSelection.context?.collection_slug}
+            mode={$currentSelection.mode ?? 'decade_genre'}
+            language={$currentSelection.language}
+            voices={$currentSelection.voices}
+            startRank={$currentSelection.startRank}
+            endRank={$currentSelection.endRank}
+            playbackOrder={$currentSelection.playbackOrder}
+            voicePlayMode={$currentSelection.voicePlayMode}
+            pauseMode={$currentSelection.pauseMode}
+            categoryMode="single"
+    />
 {:else}
-	<p class="text-gray-400 italic text-center mt-10">
-		{$status}
-	</p>
+    <p class="text-gray-400 italic text-center mt-10">
+        {$status}
+    </p>
 {/if}
 
+{#if $currentTrack}
+    <CarModeTrackMeta currentTrack={$currentTrack}/>
+
+{:else}
+    <p class="text-gray-500 text-center mt-6">
+        Waiting for track…
+    </p>
+{/if}
