27a27c1af04e733951272f9dfeec3ae67dde3df9
garysteele777 <155407816+garysteele777@users.noreply.github.com>
2026-09-05 10:09:20 -0500

    privacy: disable PostHog session replay

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,7 +5,8 @@ export const load = async () => {
     if (browser) {
         posthog.init('phc_mQ9iZBjF46F7TSgopnPcZ7VWq358b6wS9xpCBrZHJuje', {
             api_host: 'https://us.i.posthog.com',
-            defaults: '2026-05-30'
+            defaults: '2026-05-30',
+            disable_session_recording: true
         });
     }
 };
